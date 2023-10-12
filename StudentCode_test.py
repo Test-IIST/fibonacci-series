7bf801ada04fdcd5e1c13e1b1f34faa87353d974
spydie30 <113959766+spydie30@users.noreply.github.com>
2023-10-12 23:30:19 +0530

    added explaination comment

--- StudentCode_test.py
+++ StudentCode_test.py
@@ -5,4 +5,5 @@ expectedIO = IOWrapper()
 def test_hello():
     expectedIO.print("Hello World")
     assert SCOPP_tester.test(testIO=testIO , expectedIO=expectedIO)
+    # assert SCOPP_tester.test(arg1, agr 2, testIO=testIO , expectedIO=expectedIO) arg1 and arg2 will be passed to runner function
     print("Passed")
